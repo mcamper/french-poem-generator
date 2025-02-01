02c4b1f07140888e0d83249a4eb8d05acea643b9
mariacamper <mariathetech180@gmail.com>
2025-02-01 14:24:29 -0500

    Improved UX

--- src/index.js
+++ src/index.js
@@ -21,6 +21,10 @@ function generateForm (event){
     let prompt = `User instructions are: Generate a French poem about ${instructionsInput.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class= "generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
     console.log("Generating poem");
     console.log(`Prompt: ${prompt}`);
     console.log(`Context: ${context}`);
