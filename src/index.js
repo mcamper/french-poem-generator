c5a0044c831a23ab86501971dc8839a8c33893b1
mariacamper <mariathetech180@gmail.com>
2025-02-01 12:54:45 -0500

    Added typewriter.js and form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+function generateForm (event){
+    event.preventDefault();
 
+    new Typewriter('#poem', {
+        strings: "La la la la la bamba",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+    
+}
+
+
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generateForm);
